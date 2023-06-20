1a7f0246a662d7ef72ebc362fc8d382ca0923f81
Aasemaaneh <aasemaaneh8@gmail.com>
2023-06-20 18:24:11 -0400

    Pair Programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
 
+
+const transpose = require("./matrixTranspose");
+const wordSearch = (letters, word) => { 
+    if (letters.length !== 0) {
+      const horizontalJoin = letters.map(ls => ls.join(''));
+      const verticalJoin = transpose(letters).map(ls => ls.join(''));
+      for (l of horizontalJoin.concat(verticalJoin)) {
+           if (l.includes(word)){
+              return true;
+           } 
+            
+      }
+      
+      return false; 
+    } else {
+      return ('The array is empty!');
+    }
+    
+}
 module.exports = wordSearch
+
+
+
